02a7bf77368805c847814526af7a471e2fefa0aa
CodingWon <kingcoding2298@gmail.com>
2024-04-05 00:02:22 +0900

    08_Spring MVC 구성요소 만들기

---
+++ ch2/src/main/java/com/fastcampus/ch2/mapper/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.fastcampus.ch2.mapper;
+
+import java.util.List;
+
+import com.fastcampus.ch2.entity.Board;
+
+public interface BoardMapper {
+
+	public List<Board> getLists();		// 전체 리스트
+}
